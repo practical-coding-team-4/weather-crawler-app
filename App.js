8e2188652f1b087396daad99b83396d85e920fcf
Jinwoo Hwang <hnan1118@ajou.ac.kr>
2019-07-23 17:44:40 +0900

    30. Seperate components

--- App.js
+++ App.js
@@ -1,66 +1,13 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, FlatList } from 'react-native';
+import CityList from './CityList';
 import { Constants } from 'expo';
 
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            citites: [],
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
-            .then(response=>response.json())
-            .then(cities => {
-                console.log('cities =', cities.length);
-                this.setState({
-                    cities
-                });
-            });
-    }
-    onPressCity(item) {
-        console.log('onPressCity =', item);
-    }
-
-    renderItem(city) {
-        return (
-            <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
-                <Text style={styles.text}>{city}</Text>
-            </TouchableOpacity>
-        );
-    }
-
     render() {
         return(
-            <FlatList style={styles.container}
-                renderItem={({ item }) => this.renderItem(item)}
-                keyExtractor={item=>item}
-                data = {this.state.cities}
-            />
+            <CityList />
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-
-        borderColor: 'orange',
-
-        marginTop: Constants.statusBarHeight,
-        borderWidth: 1,
-    },
-    item: {
-        fontSize : 20,
-        textAlign: 'center',
-    },
-    text: {
-        fontSize: 20,
-        textAlign: 'center',
-    }
-});
---
+++ CityList.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, FlatList } from 'react-native';
+import { Constants } from 'expo';
+
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            citites: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
+            .then(response=>response.json())
+            .then(cities => {
+                console.log('cities =', cities.length);
+                this.setState({
+                    cities
+                });
+            });
+    }
+    onPressCity(item) {
+        console.log('onPressCity =', item);
+    }
+
+    renderItem(city) {
+        return (
+            <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
+                <Text style={styles.text}>{city}</Text>
+            </TouchableOpacity>
+        );
+    }
+
+    render() {
+        return(
+            <FlatList style={styles.container}
+                      renderItem={({ item }) => this.renderItem(item)}
+                      keyExtractor={item=>item}
+                      data = {this.state.cities}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Constants.statusBarHeight,
+    },
+    item: {
+        flex: 1,
+        height: 50,
+        textAlign: 'center',
+
+        borderColor: 'orange',
+        borderWidth: 1,
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+    }
+});
